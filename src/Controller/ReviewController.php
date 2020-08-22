db138fe1a605f2d157f07d337473cce9b401e8b4
JolanL67 <jolan.lazzari@gmail.com>
2020-08-22 20:31:51 +0200

    Form added, upload image, and some css

--- src/Controller/ReviewController.php
+++ src/Controller/ReviewController.php
@@ -2,18 +2,75 @@
 
 namespace App\Controller;
 
+use App\Entity\Review;
+use App\Form\ReviewType;
+use App\Repository\ReviewRepository;
+use DateTime;
+use DateTimeZone;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\Validator\Constraints\DateTime as ConstraintsDateTime;
 
 class ReviewController extends AbstractController
 {
     /**
-     * @Route("/", name="review")
+     * @Route("/", name="home")
      */
-    public function index()
+    public function home(ReviewRepository $reviewRepository)
     {
+        $reviews = $reviewRepository->findAll();
+        // dd($reviews);
+
+        return $this->render('home/index.html.twig', [
+            'reviews' => $reviews
+        ]);
+    }
+
+    /**
+     * @Route("/review", name="review")
+     */
+    public function review(Request $request, SluggerInterface $slugger)
+    {
+        $review = new Review();
+
+        $form = $this->createForm(ReviewType::class, $review);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $review = $form->getData();
+
+            //! Gérer en GMT +2
+            $review->setCreatedAt(new \DateTime());
+
+            $uploadedImage = $form['image']->getData();
+
+            if ($uploadedImage) {
+                $originalFilename = pathinfo($uploadedImage->getClientOriginalName(), PATHINFO_FILENAME);
+
+                $safeFilename = $slugger->slug($originalFilename);
+
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$uploadedImage->guessExtension();
+
+                $uploadedImage->move(
+                    $this->getParameter('avatar_directory'),
+                    $newFilename
+                );
+
+                $review->setImage($newFilename);
+            }
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($review);
+            $em->flush();
+
+            return $this->redirectToRoute('home');
+        }
+
         return $this->render('review/index.html.twig', [
-            'controller_name' => 'ReviewController',
+            'form' => $form->createView()
         ]);
     }
 }
---
+++ src/Form/ReviewType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Review;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
+
+class ReviewType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email')
+            ->add('username')
+            ->add('rating')
+            ->add('comment')
+            ->add('image', FileType::class, [
+                'constraints' => [
+                    new File([
+                        'mimeTypes' => [
+                            'image/jpg',
+                            'image/jpeg',
+                            'image/png',
+                        ],
+                    ])
+                ],
+                'required' => false,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Review::class,
+        ]);
+    }
+}
